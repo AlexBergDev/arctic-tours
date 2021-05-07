f4f2c344f24c3d3dfb1c9b7e432863bf8bc19a41
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-07 20:52:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,26 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+const resultsContainer = document.querySelector(".results");
 
-    burger.addEventListener("click", () =>  {
+const url = "Insert URL here";
 
-        //Toggle navigation
-        nav.classList.toggle("nav-active");
+async function fetchPosts() {
 
-        //Animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-            }
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        resultsContainer.innerHTML = "";
+
+        const posts = json.results;
+
+        posts.forEach(function(post) {
+            resultsContainer.innerHTML += ``;
         });
-
-        //Burger animation
-        burger.classList.toggle("toggle");
-
-    });
+      
+    }
+    catch(error) {
+        resultsContainer.innerHTML = message("error", error);
+    }
+    
 }
 
-const app = () =>{
-    navSlide();
-}
-
-app();
+fetchPosts();
