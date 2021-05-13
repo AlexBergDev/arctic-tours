703c2a44eba5d060e80eadf75f9e23d88ce0efb4
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-13 18:25:03 +0200

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,26 +1,40 @@
-const resultsContainer = document.querySelector(".results");
+const resultsContainer = document.querySelector(".row");
 
-const url = "Insert URL here";
+const url = "https://alexberg.de/api/arctic-tours/wp-json/wc/store/products";
 
 async function fetchPosts() {
 
     try {
         const response = await fetch(url);
-        const json = await response.json();
+        const getResults = await response.json();
 
         resultsContainer.innerHTML = "";
 
-        const posts = json.results;
-
-        posts.forEach(function(post) {
-            resultsContainer.innerHTML += ``;
-        });
-      
+        createHTML(getResults);
     }
+
     catch(error) {
+        console.log(error);
         resultsContainer.innerHTML = message("error", error);
     }
     
 }
 
 fetchPosts();
+
+function createHTML(posts) {
+    posts.forEach(function(post) {
+        console.log(post);
+        resultsContainer.innerHTML += `<div class="col col-1">
+                                            <div class="box">
+                                                <h3 class="content">${post.name}</h3>
+                                                <p class="content">${post.description}</p> 
+                                                <a class="button" href="#">Read More</a>
+                                            </div>
+                                        </div>
+
+                                        <div class="col post-image">
+                                            <img src="${post.images[0].src}" alt="${post.name}">
+                                        </div>`;
+    })
+}
