f41f21ffb80c9ee92aea00768d098d7d48f4150a
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-13 21:27:54 +0200

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,4 +1,4 @@
-const detailContainer = document.querySelector(".post");
+const resultsContainer = document.querySelector(".row");
 
 const queryString = document.location.search;
 
@@ -6,28 +6,42 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
+const url = "https://alexberg.de/api/arctic-tours/wp-json/wc/store/products/" + id; 
 
-const url = "Insert URL here" + id + "Insert key here";
-
-async function fetchPost() {
+async function fetchPosts() {
 
     try {
         const response = await fetch(url);
         const details = await response.json();
 
+        resultsContainer.innerHTML = "";
 
-        createHtml(details);
-      
+        createHTML(details);
     }
+
     catch(error) {
-        detailContainer.innerHTML = message("error", error);
+        resultsContainer.innerHTML = message("error", error);
     }
     
 }
 
-fetchPost();
+fetchPosts();
 
-function createHtml(details) {
-    detailContainer.innerHTML = ``;
-                                
+function createHTML(details) {
+        resultsContainer.innerHTML += `<h2>${details.name}</h2>
+                                        <div class="row">
+                                            <div class="col">
+                                                <img src="${details.images[0].src}" alt="${details.name}">
+                                            </div>
+
+                                            <div class="col-3">
+                                                <div class="box">
+                                                    <p class="content">${details.description}</p>
+                                                    <p class="content">${details.description}</p>
+                                                    <p class="content">Source: Unsplash.com</p>
+                                                    <p class="author">Author: Jane Doe</p>
+                                                    <p class="date">Posted: 23-05-2021</p>
+                                                </div>
+                                            </div>
+                                        </div>`;
 }
