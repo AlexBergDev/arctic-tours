0b7c01845c49cabce75549e53024f5c0cdac0292
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-13 21:27:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,21 @@
-const resultsContainer = document.querySelector(".results");
+const resultsContainer = document.querySelector(".row");
+const carouselContainer = document.querySelector(".slider");
 
-const url = "https://alexberg.de/api/arctic-tours/wp-json/wp/v2/posts";
+const url = "https://alexberg.de/api/arctic-tours/wp-json/wc/store/products";
 
 async function fetchPosts() {
 
     try {
         const response = await fetch(url);
-        const json = await response.json();
-
-        console.log(json);
+        const results = await response.json();
 
         resultsContainer.innerHTML = "";
+        carouselContainer.innerHTML = "";
 
-        const posts = json.results;
-
-        posts.forEach(function(post) {
-            resultsContainer.innerHTML += `<div class="col col-1">
-                        <div class="box">
-                            <h3 class="content">${post.title}</h3>
-                            <p class="content">${post.content}</p>
-                            <a class="button" href="#">Read More</a>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        <img src="images/first.jpg" alt="first" class="first">
-                    </div>`;
-        });
-      
+        createHTML(results);
+        createSlider(results)
     }
+
     catch(error) {
         resultsContainer.innerHTML = message("error", error);
     }
@@ -36,3 +23,28 @@ async function fetchPosts() {
 }
 
 fetchPosts();
+
+function createHTML(posts) {
+    posts.forEach(function(post) {
+        resultsContainer.innerHTML += `<div class="col col-1">
+                                            <div class="box">
+                                                <h3 class="content">${post.name}</h3>
+                                                <p class="content">${post.description}</p> 
+                                                <a class="button" href="post.html?id=${post.id}">Read More</a>
+                                            </div>
+                                        </div>
+
+                                        <div class="col post-image">
+                                            <img src="${post.images[0].src}" alt="${post.name}">
+                                        </div>`;
+    })
+}
+
+function createSlider(slider) {
+    slider.forEach(function(slider) {
+        carouselContainer.innerHTML += `<section class="hero slider-1">
+                                            <h1>${slider.name}</h1>
+                                            <a class="hero-button" href="post.html?id=${slider.id}">Read More</a>
+                                        </section>`;
+    })
+}
