6af702d64c13bf876845071ccbfe2d7adf48a8dd
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-13 18:25:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const resultsContainer = document.querySelector(".results");
 
-const url = "Insert URL here";
+const url = "https://alexberg.de/api/arctic-tours/wp-json/wp/v2/posts";
 
 async function fetchPosts() {
 
@@ -8,12 +8,24 @@ async function fetchPosts() {
         const response = await fetch(url);
         const json = await response.json();
 
+        console.log(json);
+
         resultsContainer.innerHTML = "";
 
         const posts = json.results;
 
         posts.forEach(function(post) {
-            resultsContainer.innerHTML += ``;
+            resultsContainer.innerHTML += `<div class="col col-1">
+                        <div class="box">
+                            <h3 class="content">${post.title}</h3>
+                            <p class="content">${post.content}</p>
+                            <a class="button" href="#">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <img src="images/first.jpg" alt="first" class="first">
+                    </div>`;
         });
       
     }
