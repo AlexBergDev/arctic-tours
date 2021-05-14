5b73b463632f9d05f1eeca2827065ac0149473d6
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-14 10:16:36 +0200

    Update post.js

--- js/post.js
+++ js/post.js
@@ -30,18 +30,32 @@ fetchPosts();
 function createHTML(details) {
         resultsContainer.innerHTML += `<h2>${details.name}</h2>
                                         <div class="row">
+
                                             <div class="col">
                                                 <img src="${details.images[0].src}" alt="${details.name}">
                                             </div>
 
                                             <div class="col-3">
                                                 <div class="box">
-                                                    <p class="content">${details.description}</p>
-                                                    <p class="content">${details.description}</p>
-                                                    <p class="content">Source: Unsplash.com</p>
-                                                    <p class="author">Author: Jane Doe</p>
+                                                    ${details.short_description}
+                                                    ${details.description}
+                                                    <p class="content">Category: ${details.categories[0].name}</p>
+                                                    <p class="author">Author: ${details.tags[0].name}</p>
                                                     <p class="date">Posted: 23-05-2021</p>
                                                 </div>
                                             </div>
+
+                                            <div class="col">
+                                                <img src="${details.images[1].src}" alt="${details.name}">
+                                            </div>
+
+                                            <div class="col">
+                                                <img src="${details.images[2].src}" alt="${details.name}">
+                                            </div>
+
+                                            <div class="col">
+                                                <img src="${details.images[3].src}" alt="${details.name}">
+                                            </div>
+                                            
                                         </div>`;
 }
