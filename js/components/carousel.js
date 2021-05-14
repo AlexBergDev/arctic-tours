5a33315ce723d7b67ee48d1c7087a856f1f15709
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-14 15:56:53 +0200

    Update carousel.js

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -1,32 +1,26 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-const slider = document.querySelector('.slider');
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-const indicatorParent = document.querySelector('.control ul'); 
-const indicators = document.querySelectorAll('.control li');
-index = 0;
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-indicators.forEach((indicator, i) => {
-  indicator.addEventListener('click', () => {
-    document.querySelector('.control .selected').classList.remove('selected');
-    indicator.classList.add('selected');
-    slider.style.transform = 'translateX(' + (i) * -25 + '%)';  
-    index = i;
-    
-  });
-});
-
-left.addEventListener('click', function() {
-  index = (index > 0) ? index -1 : 0;
-  document.querySelector('.control .selected').classList.remove('selected');
-  indicatorParent.children[index].classList.add('selected');
-  slider.style.transform = 'translateX(' + (index) * -25 + '%)';
-});
-
-right.addEventListener('click', function() {
-  index = (index < 4 - 1) ? index+1 : 3;
-  document.querySelector('.control .selected').classList.remove('selected');
-  indicatorParent.children[index].classList.add('selected');
-  slider.style.transform = 'translateX(' + (index) * -25 + '%)';
-});
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
