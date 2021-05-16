7ee3ee51b3011c379a98cb6807a1bd21b3a70b1e
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-16 13:42:31 +0200

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,6 @@
 const resultsContainer = document.querySelector(".row");
-btnMore = document.getElementById('more');
 
-let length = 5;
-
-const url = "https://alexberg.de/api/arctic-tours/wp-json/wc/store/products?per_page=" + length;
+const url = "https://alexberg.de/api/arctic-tours/wp-json/wc/store/products?per_page=12";
 
 async function fetchPosts() {
 
@@ -14,9 +11,7 @@ async function fetchPosts() {
         resultsContainer.innerHTML = "";
 
         createHTML(getResults);
-        btnMore.addEventListener('click', function () {
-            length += 5;
-        });
+        
     }
 
     catch(error) {
@@ -29,16 +24,49 @@ fetchPosts();
 
 function createHTML(posts) {
     posts.forEach(function(post) {
-        resultsContainer.innerHTML += `<div class="col col-1">
-                                            <div class="box">
-                                                <h3 class="content">${post.name}</h3>
-                                                ${post.short_description}
-                                                <a class="button" href="post.html?id=${post.id}">Read More</a>
+        resultsContainer.innerHTML += `<div class="item">
+                                            <div class="col col-1">
+                                                <div class="box">
+                                                    <h3 class="content">${post.name}</h3>
+                                                    ${post.short_description}
+                                                    <a class="button" href="post.html?id=${post.id}">Read More</a>
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div class="col post-image">
-                                            <a href="post.html?id=${post.id}"><img src="${post.images[0].src}" alt="${post.name}"></a>
+                                            <div class="col post-image">
+                                                <a href="post.html?id=${post.id}"><img src="${post.images[0].src}" alt="${post.name}"></a>
+                                            </div>
                                         </div>`;
     })
 }
+
+
+var work = document.querySelector("#workOuterShell");
+var items = Array.from(work.querySelectorAll(".item"));
+var loadMore = document.getElementById("loadMore");
+maxItems = 4;
+loadItems = 4;
+hiddenClass = "hiddenStyle";
+hiddenItems = Array.from(document.querySelectorAll(".hiddenStyle"));
+
+items.forEach(function (item, index) {
+  console.log(item.innerText, index);
+  if (index > maxItems - 1) {
+    item.classList.add(hiddenClass);
+  }
+});
+
+loadMore.addEventListener("click", function () {
+  [].forEach.call(document.querySelectorAll("." + hiddenClass), function (
+    item,
+    index
+  ) {
+    if (index < loadItems) {
+      item.classList.remove(hiddenClass);
+    }
+
+    if (document.querySelectorAll("." + hiddenClass).length === 0) {
+      loadMore.style.display = "none";
+    }
+  });
+});
