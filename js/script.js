4a2c9afb426957f1af30b6ee6bc47d50b08ab7c1
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-05-18 15:22:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,40 +25,11 @@ fetchPosts();
 
 function createSlider(slider) {
     slider.forEach(function(sliders) {
-        carouselContainer.innerHTML += `<div class="mySlides fade">
+        carouselContainer.innerHTML += `<div class="slideElements fade">
                                             <a href="post.html?id=${sliders.id}">
-                                            <img src="${sliders.images[0].src}" style="width:100%">
+                                            <img src="${sliders.images[0].src}">
                                             <h1 class="title">${sliders.name}</h1>
                                             </a>
                                         </div>`;
     })
-}
-
-function show() {
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    function plusSlides(n) {
-    showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-    showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    }
 }
